2decf88da0ae8a09f6af4773c31bd97e9ca9af9f
AtharvaBhore <atharvabhore6@google.com>
2024-04-06 18:37:39 +0530

    edit Profile functionality added
    to do :- UI design(Indrajeet)

--- src/sportify/Profile.java
+++ src/sportify/Profile.java
@@ -144,7 +144,12 @@ public class Profile extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+
+        editProfile editProfileFrame = new editProfile(email);
+        editProfileFrame.setVisible(true);
+        editProfileFrame.pack();
+        editProfileFrame.setLocationRelativeTo(null);
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
